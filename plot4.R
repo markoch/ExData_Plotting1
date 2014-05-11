e45ead44abaf750a49e8a67c9c83caabd1351924
Marco Koch <mkoch@marco-koch.net>
2014-05-12 01:32:49 +0200

    Fixing write png

--- plot4.R
+++ plot4.R
@@ -22,11 +22,13 @@ Sub_metering_1<-testData$Sub_metering_1
 Sub_metering_2<-testData$Sub_metering_2
 Sub_metering_3<-testData$Sub_metering_3
 
+png(filename = "plot4.png",  width = 480, height = 480);
 par(mfrow=c(2,2));
-png(filename = "plot4_test.png",  width = 480, height = 480);
 plot(datetime, kilowatts,  type="l",xlab="", ylab="Global Active Power");
 plot(datetime, voltage,  type="l",xlab="datetime", ylab="Voltage");
-plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering");
+plot(datetime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+points(datetime, Sub_metering_2, type="l", col="red")
+points(datetime, Sub_metering_3, type="l", col="blue")
 plot(datetime, GlobalReactivePower,  type="l",xlab="datetime", ylab="global_reactive_power");
 dev.off();
 
