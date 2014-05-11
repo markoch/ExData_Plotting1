4d8728a1861e06decdef41978e9451a3360c7798
Marco Koch <mkoch@marco-koch.net>
2014-05-12 00:27:43 +0200

    Fixing labels

--- plot2.R
+++ plot2.R
@@ -17,4 +17,4 @@ colnames(testData) <- colNames
 datetime<-as.POSIXct(paste(testData$Date,testData$Time))
 kilowatts <- as.numeric(testData$Global_active_power)
 
-plot(datetime, kilowatts,  type="l",xlab="", ylab="Gloal Active Power (kilowatts)")
+plot(datetime, kilowatts,  type="l",xlab="", ylab="Global Active Power (kilowatts)")
